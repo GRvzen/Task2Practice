b7725440e4e97cc4468f1df8011299d9b3a5ba4e
Григорий Савушкин <gr732_sagda@lab-308-7.tomtit-tomsk.ru>
2026-04-25 14:25:18 +0700

    Изменения в переменных

--- Main.gd
+++ Main.gd
@@ -3,8 +3,8 @@ extends Node
 var save_path = "user://stats.save"
 
 # Данные для статистики
-var levels_completed_overall: int = 0
-var total_attempts_on_levels: int = 0
+var levels_completed: int = 0
+var total_attempts: int = 0
 var selected_level_id: int = 1
 
 func _ready():
